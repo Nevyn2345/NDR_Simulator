ceccf6a5b472e969638182fa4676b6f0585a611e
Nevyn2345 <sbarnett1@sheffield.ac.uk>
2018-01-10 16:25:36 +0000

    Create Molecule class with trajectories

---
+++ Molecule.m
@@ -0,0 +1,29 @@
+classdef Molecule
+    properties
+        on
+        trajectory
+    end
+    methods
+        function m = Molecule(nsteps)
+            on = 1;
+            m.trajectory = zeros(nsteps,1);
+            t = 0;
+            for i = 1:nsteps
+                num = rand();
+                if t == 0
+                    if num < 0.0005
+                        m.trajectory(i) = 1;
+                        t = 1;
+                    end
+                else
+                    if num < 0.01
+                        m.trajectory(i) = 0;
+                        t = 0;
+                    else
+                        m.trajectory(i) = 1;
+                    end
+                end
+            end
+        end
+    end
+end
